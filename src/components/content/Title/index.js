4028308d6177422adfc89087d8ef8ed52089cca3
Amanda Albuquerque <amandlei@hotmail.com>
2019-09-27 00:18:43 -0300

    feat(lab): add Title component

---
+++ src/components/content/Title/index.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import Text from '../Text'
+
+const typeTitle = (element, { displayName, ...textProps}) =>
+  ({ children, ...props }) => {
+    const Title = Text.withComponent(element)
+    Title.displayName = displayName
+
+    return (
+      <Title {...textProps} {...props}>
+        {children}
+      </Title>
+    )
+  }
+
+const H1 = typeTitle('h1', {
+  displayName: 'H1',
+  fontSize: 44,
+  fontWeight: 'bold',
+  lineHeight: 1,
+  color: '#1F2D30'
+})
+
+H1.displayName = 'H1'
+
+const H2 = typeTitle('h2', {
+  displayName: 'H2',
+  fontSize: 24,
+  fontWeight: 'normal',
+  lineHeight: 1.22,
+  color: '#1F2D30'
+})
+
+H2.displayName = 'H2'
+
+const H3 = typeTitle('h3', {
+  displayName: 'H3',
+  fontSize: 21,
+  fontWeight: 'bold',
+  lineHeight: 0.95,
+  color: '#1F2D30'
+})
+
+H3.displayName = 'H3'
+
+const H4 = typeTitle('h4', {
+  displayName: 'H4',
+  fontSize: 18,
+  fontWeight: 'bold',
+  lineHeight: 1.39,
+  color: '#1F2D30'
+})
+
+H4.displayName = 'H4'
+
+const Title = { H1, H2, H3, H4 }
+
+export default Title
---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+//content
+export { default as Button } from './content/Button'
+export { default as Text } from './content/Text'
+export { default as Title } from './content/Title'
+
+// form
+export { default as Select } from './form/Select'
+
+// layout
+export { default as Flexbox } from './layout/Flexbox'
+export { default as Page } from './layout/Page'
